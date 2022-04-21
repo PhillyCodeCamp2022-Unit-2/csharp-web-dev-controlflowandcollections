69a057a0d2cb4e29bd91edf0da4cc60e0f210251
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-04-21 13:20:35 -0500

    Finished String practice

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,14 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            string rhyme = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+            // rhyme.split(' ');
+            string[] splitRhyme = rhyme.Split(". ");
+            //Console.WriteLine(string.Join(',', splitRhyme));
+            foreach (string s in splitRhyme)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
