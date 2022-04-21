d0a00ae1fef595666f03b099d31ea020d575029b
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-04-21 15:53:27 -0500

    A little dicionary practice

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
@@ -6,7 +7,23 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // The keys should be the ID numbers (as integers) and the values should be the names
+            Dictionary<int, string> students = new Dictionary<int, string>();
+
+            // key: 1, value: "Sam"
+            students[1] = "Sam";
+            // students.Add(1, "Sam"); is the same thing
+            // key: 2, value: "Francesca"
+            students[2] = "Francesca";
+            // students.Add(2, "Francesca");
+            students.Add(0, "Pearce");
+
+            Console.WriteLine(students);
+
+            foreach (KeyValuePair<int, string> kvp in students)
+            {
+                Console.WriteLine($"ID: {kvp.Key} Name: {kvp.Value}");
+            }
         }
     }
 }
