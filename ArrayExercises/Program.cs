1dd5e5016619eac7db3617983a7d7491da5f5c27
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-04-21 12:56:29 -0500

    Finished Arrays

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,21 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            // 1,1,2,3,5,8
+            int[] integerArray = { 1, 1, 2, 3, 5, 8 };
+            Console.WriteLine(integerArray);
+            //for (int i = 0; i < integerArray.Length; i++)
+            //{
+            //    Console.WriteLine(integerArray[i]);
+            //}
+
+            foreach (int integer in integerArray)
+            {
+                if (integer % 2 == 1)
+                {
+                    Console.WriteLine(integer);
+                }
+            }
         }
     }
 }
